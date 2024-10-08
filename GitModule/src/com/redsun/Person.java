03332ce903b8e2a84ea8fbdf9ad2e6c98ae723d9
wangxs <12233344.163.com>
2024-10-08 15:13:11 +0800

    增加了age属性

---
+++ GitModule/src/com/redsun/Person.java
@@ -0,0 +1,11 @@
+package com.redsun;
+
+/**
+ * @Auther: wangjie
+ * @Date: 2024/10/8 - 10 - 08 - 15:10
+ * @Description: com.redsun
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
